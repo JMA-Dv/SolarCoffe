b5911b159bb357a18f9637cc934148efbb64821f
JesusMata <alejandroavila98@hotmail.com>
2023-04-03 16:10:41 -0600

    Created InventoryController

---
+++ SolarCoffe/SolarCoffe.Api/Controllers/InventoryController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SolarCoffe.Service.Inventoies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolarCoffe.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly IInventoryService _inventory;
+        private readonly ILogger<InventoryController> _logger;
+
+        public InventoryController(IInventoryService inventory, ILogger<InventoryController> logger)
+        {
+            _inventory = inventory;
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetCurrentInventory()
+        {
+            _logger.LogInformation("Gerring all inventory");
+            return Ok();
+        }
+
+    }
+}
--- SolarCoffe/SolarCoffe.Api/Controllers/OrderController.cs
+++ SolarCoffe/SolarCoffe.Api/Controllers/OrderController.cs
@@ -1,15 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SolarCoffe.Api.Serialization;
 using SolarCoffe.Api.ViewModels;
 using SolarCoffe.Service.Customers;
 using SolarCoffe.Service.Orders;
 using SolarCoffe.Service.Products;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SolarCoffe.Api.Controllers
 {
@@ -19,28 +14,47 @@ namespace SolarCoffe.Api.Controllers
     {
         private readonly ILogger<OrderController> _logger;
 
-        private readonly IProductService _productService;
         private readonly ICustomerService _customerService;
         private readonly IOrderService _orderService;
 
-        public OrderController(ILogger<OrderController> logger, IProductService productService, ICustomerService customerService, IOrderService orderService)
+        public OrderController(ILogger<OrderController> logger, ICustomerService customerService, IOrderService orderService)
         {
             _logger = logger;
-            _productService = productService;
             _customerService = customerService;
             _orderService = orderService;
         }
 
         [HttpPost("invoice")]
-        public ActionResult GenerateNewOrder(InvoiceModel invoice)
+        public IActionResult GenerateNewOrder(InvoiceModel invoice)
         {
             _logger.LogInformation("Generating invoice");
             var order = OrderMapper.SerializeInvoiceOrder(invoice);
 
 
             order.Customer = _customerService.GetById(invoice.CustomerId).Data;
+             
             _orderService.GenerateOrderInvoice(order);
             return Ok();
+        }
+
+
+        [HttpGet]
+        public IActionResult GetOrders()
+        {
+            var orders = _orderService.GetOrders();
+
+            var orderModels = OrderMapper.SerializeorderToViewModel(orders);
+            return Ok(orderModels);
+
+        }            
+         
+        [HttpPatch("{id}")]
+        public IActionResult MarkAsComplete(int id)
+        {
+            _logger.LogInformation("Maring order as complete");
+            var response = _orderService.MarkFulfolled(id);
+
+            return Ok(response);
         }
     }
 }
