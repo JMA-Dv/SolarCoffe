fbd3b71204588058b848adcc4801c933bd6b9a18
Jesus Mata <jesus.mata@lnfdist.com>
2023-04-10 09:46:59 -0600

    Added logo, sideMenu,and components

---
+++ SolarCoffe/solarcoffe.web/src/components/SolarBtn.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="btn-link">
+        <button :class="['solar-btn', { 'full-width': isFullWidth }]" v-on:click="visitRoute">
+            <slot></slot>
+        </button>
+    </div>
+</template>
+
+<script lang="ts">
+import Component from 'vue-class-component';
+import Vue from 'vue';
+import { Prop } from 'vue-property-decorator';
+
+
+@Component({
+    name: 'SolarBtn',
+    components: {}
+})
+
+export default class SolarBtn extends Vue {
+    @Prop({ required: false, type: String })
+    link?: string;
+    @Prop({ required: false, type: Boolean, default: false })
+    isFullWidth?: string;
+
+    //TODO: Replace prop link to 'to'
+    visitRoute(): void {
+        this.$router.push(this.link);
+    }
+
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/scss/global.scss";
+
+.solar-btn {
+    background-color: lighten($color: $solar-blue, $amount: 10%);
+    color: white;
+    padding: 0.8rem;
+    transition: background-color 0.5s;
+    margin: 0.3rem 0.2rem;
+    display: inline-block;
+    cursor: pointer;
+    font-size: 1rem;
+    min-width: none;
+    border-radius: 2px solid darken($solar-blue, 20%);
+    border-radius: 3px;
+}
+
+
+.btn-link {
+    width: 100%;
+}
+
+.full-width {
+    display: block;
+    width: 100%;
+}
+</style>
---
+++ SolarCoffe/solarcoffe.web/src/views/Inventory.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="inventory-container">
+        <h1 class="inventoryTitle">
+            Inventory Dashboard
+        </h1>
+    </div>
+</template>
+
+<script lang="ts">
+import Component from 'vue-class-component';
+import Vue from 'vue';
+
+@Component({
+    name: 'Inventory',
+    components: {}
+})
+
+export default class Inventory extends Vue {
+
+}
+</script>
+
+<style scoped>
+.inventoryTitle {
+    font-size: 30px;
+}
+</style>
