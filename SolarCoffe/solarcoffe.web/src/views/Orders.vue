6d70e7b10b0f8bf5a7ad3b0a37b9f18b9a4c967b
JesusMata <alejandroavila98@hotmail.com>
2023-05-09 19:25:24 -0600

    - Added SalesOrders Entity - Modified ICustomer Typo - Added Orders Service

--- SolarCoffe/solarcoffe.web/src/views/Orders.vue
+++ SolarCoffe/solarcoffe.web/src/views/Orders.vue
@@ -1,16 +1,34 @@
 <template>
-    <h1>
-        orders
-    </h1>
+    <div>
+        <h1 id="ordersTitle">
+            Sales Orders
+        </h1>
+        <hr>
+        <table id="sales-orders" class="table" v-if="orders.length">
+            <thead>
+                <tr>Customer</tr>
+                <tr>Is Paid
+
+                </tr>
+                <tr>LineItems</tr>
+            </thead>
+
+
+        </table>
+    </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
-
+import { ISalesOrder } from '@/types/SalesOrder';
 
 @Component({
     name: 'Orders'
 })
-export default class Order extends Vue { }
+export default class Order extends Vue {
+    orders: ISalesOrder[] = [];
+}
 </script>
+
+<style lang="scss"></style>
